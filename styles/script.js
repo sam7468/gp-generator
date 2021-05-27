e608a66869f74d684c858422a5ea9b99ba9beeab
sam7468 <75119000+sam7468@users.noreply.github.com>
2021-05-27 12:02:02 +0530

    Add files via upload

---
+++ styles/script.js
@@ -0,0 +1,9 @@
+function create() {
+    var img = document.createElement('img')
+    img.setAttribute('src' , "https://media1.tenor.com/images/9a94ccad1586c72f01020b24c2dae034/tenor.gif?itemid=21659407")
+    document.getElementById('fleximgs').appendChild(img) 
+}
+
+function remove(){
+    document.getElementById('fleximgs').remove()   
+}
