2b92b9f1a0b9803a78e3f7fc03befd7ddf76df40
sam7468 <75119000+sam7468@users.noreply.github.com>
2021-05-27 12:37:13 +0530

    Update script.js

--- styles/script.js
+++ styles/script.js
@@ -1,4 +1,4 @@
-function create() {
+hfunction create() {
     var img = document.createElement('img')
     img.setAttribute('src' , "https://media1.tenor.com/images/9a94ccad1586c72f01020b24c2dae034/tenor.gif?itemid=21659407")
     document.getElementById('fleximgs').appendChild(img) 
@@ -8,7 +8,7 @@ function remove(){
     document.getElementById('fleximgs').remove()
     var h3 = document.createElement('h3')
     var text = document.createTextNode("Refresh you page!")
-    h3.setAttribute('id' , 'refresh')
+    h3.setAttribute('id' , 'id2')
     h3.appendChild(text)
-    document.getElementById("warning").appendChild(h3)
+    document.getElementById("refresh").appendChild(h3)
 }
