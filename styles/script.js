70798431bce3cfee4fa5ff406548a109c1d9f410
sam7468 <75119000+sam7468@users.noreply.github.com>
2021-05-27 14:03:40 +0530

    Update script.js

--- styles/script.js
+++ styles/script.js
@@ -8,7 +8,7 @@ function remove(){
     
     document.getElementById('fleximgs').remove()   
     var h3 = document.createElement('h3')
-    var text = document.createTextNode("Refresh you page!")
+    var text = document.createTextNode("Refresh your page!")
     h3.setAttribute('id' , 'warningtext')
     h3.appendChild(text)
     document.getElementById("warning").appendChild(h3)
